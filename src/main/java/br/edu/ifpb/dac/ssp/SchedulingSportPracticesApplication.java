4ff1abbcdd7f860d8614fbe3f7eb5ae2dc560105
DanSousa <dansousac2@gmail.com>
2022-04-11 23:35:36 -0300

    primeiro commit: packages criados

---
+++ src/main/java/br/edu/ifpb/dac/ssp/SchedulingSportPracticesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.ssp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingSportPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingSportPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/dac/ssp/SchedulingSportPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.ssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingSportPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
