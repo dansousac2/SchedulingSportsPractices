09daaf4f579fb2b878860a77726d7ade8ddabdb5
DanSousa <dansousac2@gmail.com>
2022-04-13 11:29:43 -0300

    Test save of CRUD's Place workin now

--- src/test/java/br/edu/ifpb/dac/ssp/controller/PlaceControllerTest.java
+++ src/test/java/br/edu/ifpb/dac/ssp/controller/PlaceControllerTest.java
@@ -10,39 +10,49 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import br.edu.ifpb.dac.ssp.model.Place;
 import br.edu.ifpb.dac.ssp.model.dto.PlaceDTO;
+import br.edu.ifpb.dac.ssp.repository.PlaceRepository;
+import br.edu.ifpb.dac.ssp.service.PlaceConverterService;
 import br.edu.ifpb.dac.ssp.service.PlaceService;
 
 class PlaceControllerTest {
-/*
+
 	@InjectMocks
-	private PlaceController controller = new PlaceController();
+	@Autowired
+	private PlaceController controller;
 	@Mock
 	private PlaceService service;
+	@Mock
+	private PlaceRepository repository;
+	@Mock
+	private PlaceConverterService converter;
 	private PlaceDTO dto;
-	
+	@Captor
 	private ArgumentCaptor<Place> capPlace;
-	
-	@BeforeAll
-	public void beforeAllDo() {
-		MockitoAnnotations.openMocks(this);
-		ReflectionTestUtils.setField(controller,"placeService",service);
-	}
 	
 	@BeforeEach
 	public void dtoLauncher() {
+		MockitoAnnotations.openMocks(this);
+		ReflectionTestUtils.setField(controller, "converterService", converter);
+		ReflectionTestUtils.setField(service,"placeRepository",repository);
+		ReflectionTestUtils.setField(controller,"placeService",service);
 		dto = new PlaceDTO();
-		dto
-		// realizar sets no dto
+		dto.setId(1);
+		dto.setName("Quadra Esportiva");
+		dto.setPublic(false);
+		dto.setMaximumCapacityParticipants(20);
+		dto.setReference("Próximo à entrada");
 	}
 	
 	/*
@@ -53,21 +63,21 @@ class PlaceControllerTest {
 	 * são individuais oriundas do mesmo "save" por isso acredito que possam estar no mesmo teste
 	 */
 	
-	/*
 	@Test
 	public void saveOK() {
-		Mockito.verify(service).save(capPlace.capture());
+		// Mockito.when in first, when possible.         
+		Mockito.when(converter.dtoToPlace(Mockito.any(PlaceDTO.class))).thenCallRealMethod();
 		ResponseEntity respEntity = controller.save(dto);
+		Mockito.verify(service).save(capPlace.capture());
+		Mockito.when(repository.save(Mockito.any())).thenReturn(capPlace.getValue());
 		Place sportInDB = capPlace.getValue();
 		
 		assertEquals(dto.getName(), sportInDB.getName());
 		if(!(dto.getReference() == null)) {
-			assertEquals(dto.getReference(), sport.getReference());
+			assertEquals(dto.getReference(), sportInDB.getReference());
 		}
 		assertEquals(HttpStatus.CREATED, respEntity.getStatusCode());
-		assertEquals(null,respEntity.getBody().getDadosSensiveis01());
-		assertEquals(null,respEntity.getBody().getDadosSensiveis02());
+//		assertEquals(null,respEntity.getBody().getDadosSensiveis01());
 	}
-	*/
 
 }
