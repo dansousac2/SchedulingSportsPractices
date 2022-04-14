043b8a6585cc890c4557bc982b2afafa74b64f53
DanSousa <dansousac2@gmail.com>
2022-04-14 19:32:28 -0300

    positive tests done to PlaceController

--- src/test/java/br/edu/ifpb/dac/ssp/controller/PlaceControllerTest.java
+++ src/test/java/br/edu/ifpb/dac/ssp/controller/PlaceControllerTest.java
@@ -3,6 +3,10 @@ package br.edu.ifpb.dac.ssp.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -16,11 +20,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import br.edu.ifpb.dac.ssp.exception.MissingFieldException;
 import br.edu.ifpb.dac.ssp.model.Place;
 import br.edu.ifpb.dac.ssp.model.dto.PlaceDTO;
 import br.edu.ifpb.dac.ssp.service.PlaceConverterService;
 import br.edu.ifpb.dac.ssp.service.PlaceService;
+
 
 class PlaceControllerTest {
 
@@ -33,6 +37,7 @@ class PlaceControllerTest {
 	private Place exPlace;
 	@Captor
 	private ArgumentCaptor<Place> capPlace;
+	private ResponseEntity respEntity;
 	
 	@BeforeEach
 	public void dtoLauncher() {
@@ -49,28 +54,20 @@ class PlaceControllerTest {
 		
 		exDto = new PlaceDTO();
 		exDto.setId(1);
-		exDto.setName("Quadra Esportiva");
+		exDto.setName("Campo de Voleibol");
 		exDto.setPublic(false);
+		exDto.setReference("Perto da Entrada");
 		exDto.setMaximumCapacityParticipants(20);
-		exDto.setReference("Próximo à entrada");
 	}
-	
-	/*
-	 * Comparações a serem feitas:
-	 * 1-objeto no banco e objeto criado são o mesmo
-	 * 2-Https = ok
-	 * 3-dto não passa dados sensíveis
-	 * são individuais oriundas do mesmo "save" por isso acredito que possam estar no mesmo teste
-	 */
-	
+
 	/*
 	 * Object in DB have the same atributes (id and name, for example) as the 
 	 * DTO of the parameter's method
 	 */
 	@Test
 	public void saveObjectInDb() {
-		ResponseEntity respEntity = controller.save(exDto);
 		try {
+			respEntity = controller.save(exDto);
 			Mockito.verify(service).save(capPlace.capture());
 			Place placeDB = capPlace.getValue();
 			assertEquals(exDto.getId(), placeDB.getId());
@@ -88,7 +85,8 @@ class PlaceControllerTest {
 		
 		try {
 			Mockito.when(service.save(Mockito.any())).thenReturn(exPlace);
-			ResponseEntity respEntity = controller.save(exDto);
+			respEntity = controller.save(exDto);
+			
 			assertEquals(HttpStatus.CREATED, respEntity.getStatusCode());
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -106,12 +104,83 @@ class PlaceControllerTest {
 		exDto.setName(null);
 		try {
 			Mockito.when(service.save(Mockito.any())).thenCallRealMethod();
-			ResponseEntity resp = controller.save(exDto);
-			String sResp = String.valueOf(resp.getBody());
+			respEntity = controller.save(exDto);
+			String sResp = String.valueOf(respEntity.getBody()); // Exception is throwed and passed to de ResponseEntity 
 			
 			assertTrue(sResp.contains("name is missing") && sResp.contains("save"));
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void updateInDB() {
+		try {
+			respEntity = controller.update(1,exDto);
+			Mockito.verify(service).update(capPlace.capture());
+			Place placeDB = capPlace.getValue();
+			
+			assertEquals(exDto.getId(), placeDB.getId());
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+	
+	public void updateHttpsStatusOK() {
+		try {
+			Mockito.when(service.update(Mockito.any())).thenReturn(exPlace);
+			respEntity = controller.update(1,exDto);
+			
+			assertEquals(HttpStatus.OK, respEntity.getStatusCode());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	@Disabled
+	public void updateSomeAtributeNotMustBeShown() {
+		// verify business rule in the ConverterService
+	}
+	
+	@Test
+	public void deleteHttpStatusAndBody() {
+		respEntity = controller.delete(1);
+		
+		assertEquals(HttpStatus.NO_CONTENT, respEntity.getStatusCode());
+		assertEquals(null, respEntity.getBody());
+	}
+	
+	@Test
+	public void findById() {
+		try {
+			Mockito.when(service.findById(1)).thenReturn(exPlace);
+			respEntity = controller.findById(1);
+			
+			assertEquals(HttpStatus.OK, respEntity.getStatusCode());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void getAllHttpStatusOK() {
+		try {
+			List<Place> list = new ArrayList<>();
+			for(int i = 1; i < 4; i++) {
+				String nameComp = String.valueOf(i);
+				Place place = new Place();
+				place.setId(i);
+				place.setName("lugar" + nameComp);
+				place.setPublic(true);
+				list.add(place);
+			}
+			Mockito.when(service.findAll()).thenReturn(list);
+			respEntity = controller.getAll();
+			
+			assertEquals(HttpStatus.OK, respEntity.getStatusCode());
+		} catch (Exception e) {
+			e.getMessage();
 		}
 	}
 }
