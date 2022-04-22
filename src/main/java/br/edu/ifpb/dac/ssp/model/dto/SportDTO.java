4e6d6107d94b128e529427cb273013d9ff4ac180
DanSousa <dansousac2@gmail.com>
2022-04-22 11:36:49 -0300

    tests of SportDTO done

--- src/main/java/br/edu/ifpb/dac/ssp/model/dto/SportDTO.java
+++ src/main/java/br/edu/ifpb/dac/ssp/model/dto/SportDTO.java
@@ -1,9 +1,18 @@
 package br.edu.ifpb.dac.ssp.model.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
 public class SportDTO { 
 	
 	private Integer id;
+	@NotBlank(message = "Name not must be empty")
+	@Pattern(regexp = "^[a-zA-ZÀ-ú\\s]{3,255}$", message = "Name is invalid! Verify if there are special characters nad size 4 chars or more.")
 	private String name;
+	@Positive(message = "capacity must be positive")
+	@Max(value = 400, message = "Max value to maximum capacity participants is 400.")
 	private int minimumNumberOfPractitioners;
 	 
 	public SportDTO() {
---
+++ src/test/java/br/edu/ifpb/dac/ssp/model/dto/SportDTOTest.java
@@ -0,0 +1,55 @@
+package br.edu.ifpb.dac.ssp.model.dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SportDTOTest {
+
+	private SportDTO dto;
+	private Set<ConstraintViolation<SportDTO>> violations;
+	private static Validator validator;
+	
+	@BeforeAll
+	public static void setup() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+	
+	@BeforeEach
+	public void beforeEach() {
+		dto = new SportDTO();
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"Voleyball", " Football", "Basketball   "}) // valid
+	public void nameIsValid(String s) {
+		dto.setName(s);
+		violations = validator.validateProperty(dto, "name");
+		
+		if(violations.size() != 0) {
+			System.out.println(s + " => " + violations.stream().findFirst().get().getMessage());	
+		}
+		assertEquals(0, violations.size(), "INVALID NAME FOUND<" + s + ">");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"123", "Fu", "", "   ", "\t", "\n\n", " Futb@ll  ", " *", "   &LA ", ","}) // invalid
+	public void nameIsInvalid(String s) {
+		dto.setName(s);
+		violations = validator.validateProperty(dto, "name");
+		
+		assertNotEquals(0, violations.size(), "VALID NAME FOUND< " + s + " >");
+	}
+	
+}
