8bddc3623f7a537b222f79211850c76a5569e896
DanSousa <dansousac2@gmail.com>
2022-04-26 10:48:21 -0300

    creaed methods confirmedBySport and confirmedByPlace in SchedullingController

--- src/main/java/br/edu/ifpb/dac/ssp/controller/SchedulingController.java
+++ src/main/java/br/edu/ifpb/dac/ssp/controller/SchedulingController.java
@@ -1,6 +1,5 @@
 package br.edu.ifpb.dac.ssp.controller;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -32,109 +31,133 @@ import br.edu.ifpb.dac.ssp.service.UserService;
 @RequestMapping("api/scheduling")
 
 public class SchedulingController {
-	
+
 	@Autowired
 	private SchedulingService schedulingService;
-	
+
 	@Autowired
 	private SchedulingConverterService converterService;
-	
+
 	@Autowired
 	private SchedulingValidatorService validatorService;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private UserConverterService userConverterService;
-	
-	
+
 	@GetMapping
 	public ResponseEntity getAll() {
 		try {
 			List<Scheduling> entityList = schedulingService.findAll();
-			
+
 			List<SchedulingDTO> dtoList = converterService.schedulingToDtos(entityList);
-			
+
 			return ResponseEntity.ok().body(dtoList);
-			
+
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity findById(@PathVariable Integer id) {
 		try {
 			Scheduling entity = schedulingService.findById(id);
 			SchedulingDTO dto = converterService.schedulingToDto(entity);
-			
+
 			return ResponseEntity.ok().body(dto);
-		
+
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
+
 	@PostMapping
 	public ResponseEntity save(@RequestBody SchedulingDTO dto) {
-		
+
 		try {
 			validatorService.validateSchedulingDTO(dto);
 			Scheduling entity = converterService.dtoToScheduling(dto);
-			
+
 			validatorService.validateScheduling(entity);
 			entity = schedulingService.save(entity);
-			
+
 			dto = converterService.schedulingToDto(entity);
-			
+
 			return new ResponseEntity(dto, HttpStatus.CREATED);
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
-	
+
 	@PatchMapping("/participation/{id}")
 	public ResponseEntity addParticipant(@PathVariable Integer id, @RequestBody Integer userRegistration) {
 		try {
 			User user = userService.findByRegistration(userRegistration).get();
 			Scheduling entity = schedulingService.findById(id);
-			
+
 			entity.addParticipant(user);
 			entity = schedulingService.save(entity);
-			
+
 			UserDTO userDto = userConverterService.userToDto(user);
-			
+
 			return ResponseEntity.ok().body(userDto);
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
+
+	@GetMapping("/confirmedByPlace/{place}")
+	public ResponseEntity getAllSchedulingConfirmedByPlace(@PathVariable String place) {
+		try {
+			validatorService.validatePlaceName(place);
+			List<Scheduling> entityList = schedulingService.findAllByPlaceName(place);
+			List<SchedulingDTO> dtoList = converterService.schedulingToDtos(entityList);
+
+			return ResponseEntity.ok().body(dtoList);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@GetMapping("/confirmedBySport/{sport}")
+	public ResponseEntity getAllSchedulingConfirmedBySport(@PathVariable String sport) {
+		try {
+			validatorService.validateSportName(sport);
+			List<Scheduling> entityList = schedulingService.findAllBySportName(sport);
+			List<SchedulingDTO> dtoList = converterService.schedulingToDtos(entityList);
+
+			return ResponseEntity.ok().body(dtoList);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
 	@GetMapping("/participation/{id}")
 	public ResponseEntity getSchedulingParticipants(@PathVariable Integer id) {
 		try {
 			Scheduling entity = schedulingService.findById(id);
-			
+
 			List<User> participantList = new ArrayList<>();
 			participantList.addAll(entity.getParticipants());
-			
+
 			List<UserDTO> participantListDTO = userConverterService.usersToDtos(participantList);
-			
+
 			return ResponseEntity.ok().body(participantListDTO);
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public ResponseEntity delete(@PathVariable Integer id) {
 		try {
 			schedulingService.deleteById(id);
-			
+
 			return ResponseEntity.noContent().build();
-			
+
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
