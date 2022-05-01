c9ae6dcabb48798f7374da39950fac7b52dea26f
Keilla Bezerra <keillavfb@gmail.com>
2022-04-30 22:41:18 -0300

    Added tests for SchedulingValidatorServiceTest

--- src/test/java/br/edu/ifpb/dac/ssp/service/SchedulingValidatorServiceTest.java
+++ src/test/java/br/edu/ifpb/dac/ssp/service/SchedulingValidatorServiceTest.java
@@ -7,10 +7,14 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import br.edu.ifpb.dac.ssp.exception.ObjectNotFoundException;
 import br.edu.ifpb.dac.ssp.exception.RuleViolationException;
+import br.edu.ifpb.dac.ssp.exception.TimeAlreadyScheduledException;
 import br.edu.ifpb.dac.ssp.model.Place;
 import br.edu.ifpb.dac.ssp.model.Scheduling;
 import br.edu.ifpb.dac.ssp.model.Sport;
@@ -127,7 +132,7 @@ public class SchedulingValidatorServiceTest {
 		
 		try {
 			when(validatorService.validateScheduledTime(any(LocalTime.class), any(LocalTime.class))).thenReturn(true);
-			
+				
 			boolean isValid = validatorService.validateScheduledTime(startTime, finishTime);
 			assertTrue(isValid);
 		} catch (Exception e) {
@@ -136,60 +141,152 @@ public class SchedulingValidatorServiceTest {
 	}
 
 	@Test
-	// Esse não consegui mockar muita coisa :(
 	public void testValidateScheduledTimeException() {
 		LocalTime startTime = LocalTime.parse("05:00");
 		LocalTime finishTime = LocalTime.parse("06:00");
 		
 		try {
 			when(dateConverter.stringToTime(anyString())).thenCallRealMethod();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 			
-			Throwable exception = assertThrows(RuleViolationException.class, () -> validatorService.validateScheduledTime(startTime, finishTime));
+		Throwable exception = assertThrows(RuleViolationException.class, () -> validatorService.validateScheduledTime(startTime, finishTime));
 			
-			assertEquals("Scheduled time should be between 07:00 and 22:00", exception.getMessage());		
+		assertEquals("Scheduled time should be between 07:00 and 22:00", exception.getMessage());		
+	}
+	
+	@Test
+	public void testValidateScheduledDateAndTimeOk() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+		
+		LocalDate scheduledDate = LocalDate.now();
+		LocalTime scheduledStartTime = LocalTime.parse(LocalTime.now().format(dtf)).plusMinutes(5);
+		
+		try {
+			when(dateConverter.dateTimeNow()).thenCallRealMethod();
+			when(dateConverter.stringToDateTime(anyString())).thenCallRealMethod();
+			
+			boolean isValid = validatorService.validateScheduledDateAndTime(scheduledDate, scheduledStartTime);
+			assertTrue(isValid);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 	
-	// Ainda falta montar com mock esses:
-	
-	@Test
-	public void testValidateScheduledDateAndTimeOk() {
-		
-	}
-	
 	@Test
 	public void testValidateScheduledDateAndTimeException() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
 		
+		LocalDate scheduledDate = LocalDate.now();
+		LocalTime scheduledStartTime = LocalTime.parse(LocalTime.now().format(dtf)).minusMinutes(5);
+		
+		try {
+			when(dateConverter.dateTimeNow()).thenCallRealMethod();
+			when(dateConverter.stringToDateTime(anyString())).thenCallRealMethod();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		Throwable exception = assertThrows(RuleViolationException.class, () -> validatorService.validateScheduledDateAndTime(scheduledDate, scheduledStartTime));
+		
+		assertEquals("Scheduled date shouldn't be in past!", exception.getMessage());
 	}
 	
 	@Test
 	public void testValidateDurationOfPracticeOk() {
+		LocalTime startTime = LocalTime.parse("08:00");
+		LocalTime finishTime = LocalTime.parse("10:00");
 		
+		try {
+			boolean isValid = validatorService.validateDurationOfPractice(startTime, finishTime);
+			assertTrue(isValid);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
-
 	@Test
 	public void testValidateDurationOfPracticeExceptionFinishTimeBeforeStartTime() {
+		LocalTime startTime = LocalTime.parse("10:00");
+		LocalTime finishTime = LocalTime.parse("08:00");
 		
+		try {
+			Throwable exception = assertThrows(RuleViolationException.class, () -> validatorService.validateDurationOfPractice(startTime, finishTime));
+			assertEquals("Duration of practice shouldn't be negative or 0!", exception.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	
 	@Test
 	public void testValidateDurationOfPracticeExceptionMaximumDuration() {
+		LocalTime startTime = LocalTime.parse("08:00");
+		LocalTime finishTime = LocalTime.parse("13:00");
 		
+		try {
+			Throwable exception = assertThrows(RuleViolationException.class, () -> validatorService.validateDurationOfPractice(startTime, finishTime));
+			assertEquals("Duration of practice should be a maximum of 180 minutes!", exception.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	@Test
 	public void testValidateScheduledDateOk() {
-	
+		Scheduling entityMocked1 = mock(Scheduling.class);
+		when(entityMocked1.getScheduledStartTime()).thenReturn(LocalTime.parse("09:00"));
+		when(entityMocked1.getScheduledFinishTime()).thenReturn(LocalTime.parse("11:00"));
+
+		Place place = mock(Place.class);
+		when(place.getName()).thenReturn("Pátio");
+		
+		Scheduling entityMocked2 = mock(Scheduling.class);
+		when(entityMocked2.getScheduledStartTime()).thenReturn(LocalTime.parse("07:00"));
+		when(entityMocked2.getScheduledFinishTime()).thenReturn(LocalTime.parse("08:00"));
+		when(entityMocked2.getPlace()).thenReturn(place);
+		
+		List<Scheduling> listEntity = spy(List.class);
+		listEntity.add(entityMocked1);
+		
+		when(schedulingService.findAllByPlaceNameAndScheduledDate(anyString(), any(LocalDate.class))).thenReturn(listEntity);
+		
+		try {
+			boolean isValid = validatorService.validateScheduledDate(entityMocked2);
+			
+			assertTrue(isValid);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
+	// Esse quebrou
 	@Test
 	public void testValidateScheduledDateException() {
+		Scheduling entityMocked1 = mock(Scheduling.class);
+		when(entityMocked1.getScheduledStartTime()).thenReturn(LocalTime.parse("09:30"));
+		when(entityMocked1.getScheduledFinishTime()).thenReturn(LocalTime.parse("11:30"));
+
+		Place place = mock(Place.class);
+		when(place.getName()).thenReturn("Pátio");
 		
+		Scheduling entityMocked2 = mock(Scheduling.class);
+		when(entityMocked2.getPlace()).thenReturn(place);
+		when(entityMocked2.getScheduledStartTime()).thenReturn(LocalTime.parse("09:00"));
+		when(entityMocked2.getScheduledFinishTime()).thenReturn(LocalTime.parse("11:00"));
+		
+		List<Scheduling> listEntity = spy(List.class);
+		listEntity.add(entityMocked1);
+		
+		when(schedulingService.findAllByPlaceNameAndScheduledDate(anyString(), any(LocalDate.class))).thenReturn(listEntity);
+		
+		try {
+			Throwable exception = assertThrows(TimeAlreadyScheduledException.class, () -> validatorService.validateScheduledDate(entityMocked2));
+			assertEquals("There is already a practice scheduled for this time!", exception.getMessage());
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
-	
-	
 }
