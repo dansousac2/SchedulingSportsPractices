3aa8fedbd75b6e8f0586614ba6602b26da5d69f9
Ayanne prata <ayanneprata7@gmail.com>
2022-05-02 16:30:41 -0300

    User Converter Service Tests

---
+++ src/test/java/br/edu/ifpb/dac/ssp/service/UserConverterServiceTest.java
@@ -0,0 +1,91 @@
+package br.edu.ifpb.dac.ssp.service;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import br.edu.ifpb.dac.ssp.model.User;
+import br.edu.ifpb.dac.ssp.model.dto.UserDTO;
+
+public class UserConverterServiceTest {
+	
+	private static UserConverterService converterService;
+	private static User user;
+	private static UserDTO dto;
+	
+	@BeforeAll
+	public static void setUp() {
+		converterService = new UserConverterService();
+		user = new User();
+		
+		user.setId(1);
+		user.setName("Maria Souza");
+		user.setEmail("mariasouza@gmail.com");
+		user.setRegistration(1234);
+		user.setPassword("122");
+		
+		dto = new UserDTO();
+		dto.setId(1);
+		dto.setName("Maria Souza");
+		dto.setEmail("mariasouza@gmail.com");
+		dto.setRegistration(1234);
+		
+		
+	}
+	
+	
+	@Test
+	public void testConvertingDtoToEntity() {
+		User userConverted = converterService.dtoToUser(dto);
+		assertAll("Testing comparing dto and entity field by field",
+				() -> assertEquals(userConverted.getId(), dto.getId()),
+				() -> assertEquals(userConverted.getName(), dto.getName()),
+				() -> assertEquals(userConverted.getEmail(), dto.getEmail()),
+				() -> assertEquals(userConverted.getRegistration(), dto.getRegistration()),
+				() -> assertEquals(userConverted.getPassword(), dto.getPassword())
+		);
+	}
+	
+	
+	@Test
+	public void testConvertingEntityToDto() {
+		UserDTO dtoConverted = converterService.userToDto(user);
+		assertAll("Testing comparing dto and entity field by field",
+				() -> assertEquals(dtoConverted.getId(), user.getId()),
+				() -> assertEquals(dtoConverted.getName(), user.getName()),
+				() -> assertEquals(dtoConverted.getEmail(), user.getEmail()),
+				() -> assertEquals(dtoConverted.getRegistration(), user.getRegistration())
+								
+			);
+	}
+	
+//	@Test
+//	public void testConvertingEntityToDtoPassword() {
+//		UserDTO dtoConverted = converterService.userToDto(user);
+//		assertAll("Testing comparing dto and entity field by field",
+//				() -> assertEquals(dtoConverted.getPassword(), null)
+				
+//		   );
+
+		
+		
+//	}
+	
+	@Test
+	public void testConvertingEntityToDtoNullPointer() {
+		Throwable exception = assertThrows(NullPointerException.class, () -> converterService.userToDto(null));
+		assertEquals("Conversion impossible. the object is null", exception.getMessage());
+	}
+	
+	@Test
+	public void testConvertingDtoToEntityNullPointer() {
+		Throwable exception = assertThrows(NullPointerException.class, () -> converterService.dtoToUser(null));
+		assertEquals("Conversion impossible. the object is null", exception.getMessage());
+	}
+	
+
+
+}
